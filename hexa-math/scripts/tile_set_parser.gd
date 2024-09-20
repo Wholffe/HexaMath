946d916d60aa599e0e0aa5a06b5e904ed8df7943
Wholffe <marc2002sch@googlemail.com>
2024-09-21 01:22:31 +0200

    refactoring, rebuild script structure
    (cherry picked from commit 76cad10ae1b75e7902ab985d2927dca1f8945440)

--- hexa-math/scripts/tile_set_parser.gd
+++ hexa-math/scripts/tile_set_parser.gd
@@ -1,22 +1,24 @@
 extends Node2D
 
 @export var current_player_number: int = 99
-@onready var layer0: TileMapLayer = $Layer0
+@onready var nonplayerlayer: TileMapLayer = $NonPlayerLayer
+@onready var playerlayer: TileMapLayer = $PlayerLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Utils.player_current_number = current_player_number
 	Utils.player_move_to_current_mouse_pos.connect(Callable(self,"set_new_player_pos"))
-	var cells = layer0.get_used_cells()
-	var cell_position: Dictionary = {}
+	var cells = nonplayerlayer.get_used_cells()
+	var non_player_cell_pos: Array = []
 	for vector in cells:
-		cell_position[vector] = true
-		print(vector)
-	print(layer0.get_node_and_resource("res://grid_elements/player.tscn"))
-	print(cell_position)
+		non_player_cell_pos.append(vector)
+	print(non_player_cell_pos)
 	
 func set_new_player_pos(old_player_pos: Vector2) -> void:
-	var new_player_pos = layer0.local_to_map(get_global_mouse_position())
-	old_player_pos = layer0.local_to_map(old_player_pos)
-	layer0.set_cell(new_player_pos,0,Vector2i.ZERO,2)
-	layer0.set_cell(old_player_pos,0,Vector2i.ZERO,1)
+	var new_player_pos = playerlayer.local_to_map(get_global_mouse_position())
+	old_player_pos = nonplayerlayer.local_to_map(old_player_pos)
+	nonplayerlayer.erase_cell(new_player_pos)
+	nonplayerlayer.set_cell(old_player_pos,0,Vector2i.ZERO,1)
+	playerlayer.erase_cell(old_player_pos)
+	playerlayer.set_cell(new_player_pos,0,Vector2i.ZERO,1)
+	Utils.player_current_pos = Vector2(new_player_pos)
--- hexa-math/scripts/utils.gd
+++ hexa-math/scripts/utils.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var player_current_number: int
+var player_current_pos: Vector2
 
 signal player_add(value: int)
 signal player_move_to_current_mouse_pos(old_player_pos: Vector2)
